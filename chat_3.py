74224c1ced2c7370103df0a5657aa33ff79a4125
Bryant Chen <kobe2103@gmail.com>
2021-08-04 23:15:29 +0800

    third

---
+++ chat_3.py
@@ -0,0 +1,11 @@
+# 讀取檔案
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig') as f:
+    for line in f:
+        lines.append(line.strip())
+
+for line in lines:
+    s = line.split(' ')
+    time = s[0][:5]
+    name = s[1][5:]
+    print(name)
